0d450114815073134a2b6266c69bc70c10619953
Puipu <simon99031@gmail.com>
2024-07-21 22:28:21 +0800

    vault backup: 2024-07-21 22:28:21

--- 資結/code practice/uva514.py
+++ 資結/code practice/uva514.py
@@ -1,23 +1,51 @@
 while True:
     N = int(input())
-    print(f"N={N}")
     if N == 0:
         break
     
     while True:
-        first = int(input())
-        # print(f"first={first}")
+        # origin train
+        train = [int(t) for t in range(1, N+1)]
+        # destination
+        station = input().split()
+        station = [int(t) for t in station] # to int array
+        first = station[0]
         if first == 0:
-            print("break inside")
+            print() # null line
             break
-        train = []
-        train.append(first)
-        # push N-1 times
-        for i in range(1, N):
-            t = int(input().split())
-            train.append(t)
-            # print
-            print(f"train: {train}")
+        
+    
+        print(f"train: {train}")
+        print(f"station : {station}")
+        
+        # logic part
+        stack = []
+        idx = 0 # pointer of "train"
+        # for t in train:
+        #     stack.append(t)
+            
+        #     if (t == station[idx]):
+        #         print(f"    t={t}")
+        #         idx += 1
+        #         stack.pop()
+                
+        for st in station:
+            if idx == len(train)-1:
+                break
+            
+            stack.append(train[idx])
+            idx += 1
+            
+            if (st == train[idx]):
+                stack.pop()
+        
+        print(f"stack={stack}")   
+        if len(stack) == 0:
+            print("yes")
+        else:
+            print("no")            
+        # print
+        
         
         
         
