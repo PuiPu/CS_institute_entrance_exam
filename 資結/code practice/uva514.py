7f634868188514718e57b7072dbbfb2d671a42a6
Puipu <simon99031@gmail.com>
2024-07-21 20:35:33 +0800

    vault backup: 2024-07-21 20:35:33

--- 資結/code practice/uva514.py
+++ 資結/code practice/uva514.py
@@ -1,1 +1,20 @@
-N = input()
+while True:
+    N = input()
+
+    if N == 0:
+        break
+    
+    first = 0
+
+    while True:
+        first = input()
+        if first == 0:
+            break
+        train = []
+        train.append(first)
+        # push N-1 times
+        for i in range(1, N):
+            train.append(input())
+        # print
+        print(f"train: {train}")
+        
